d22d9413e58abf17c352fbd6042c1d9d7bc1d0ec
Yu Ding <dingelish@gmail.com>
2019-06-23 22:31:46 -0700

    Add SGX

--- src/lib.rs
+++ src/lib.rs
@@ -65,6 +65,16 @@
 //! }
 //! ```
 
+#![cfg_attr(all(feature = "mesalock_sgx",
+                not(target_env = "sgx")), no_std)]
+#![cfg_attr(all(target_env = "sgx", target_vendor = "mesalock"), feature(rustc_private))]
+
+#[cfg(all(feature = "mesalock_sgx", not(target_env = "sgx")))]
+#[macro_use]
+extern crate sgx_tstd as std;
+
+use std::prelude::v1::*;
+
 extern crate adler32;
 
 use std::cmp;
